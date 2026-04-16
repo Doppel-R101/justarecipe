f08a6475d92fb7d95b0e62688709d43134199a0e
Doppel_R <doppelr1001@gmail.com>
2026-04-16 14:38:10 +0200

    attempt at setting up workspace, change recipe syntax to match 1.20.1

--- src/main/java/dev/justablock/justarecipe/CreateJustARecipe.java
+++ src/main/java/dev/justablock/justarecipe/CreateJustARecipe.java
@@ -1,24 +1,32 @@
 package dev.justablock.justarecipe;
 
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
-
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(CreateJustARecipe.MODID)
 public class CreateJustARecipe {
     public static final String MODID = "justarecipe";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public CreateJustARecipe(IEventBus modEventBus, ModContainer modContainer) {
+    public CreateJustARecipe(FMLJavaModLoadingContext context)
+    {
+        IEventBus modEventBus = context.getModEventBus();
+
+        // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
     }
 
-    private void commonSetup(FMLCommonSetupEvent event) {
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
     }
 }
